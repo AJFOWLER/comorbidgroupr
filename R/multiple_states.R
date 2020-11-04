acae959cd8679acea5f6fcbad9257dc396977443
Alex J Fowler <alexjfowler@gmail.com>
2020-11-04 16:25:44 +0000

    empty columns are an issue, fixed here with horrendous loop

--- R/multiple_states.R
+++ R/multiple_states.R
@@ -1,34 +1,21 @@
 #' Multiple states
-#' @description
-#' @param comorbid_column A vector of character strings made up of 0s and 1s, or of factors coercible to character, all should be identical lengths.
 #'
+#' @description Sometimes, diseases may have multiple states, for example a different stage of cancer or kidney disease. This function processes comorbid strings that have multiple states
+#' to enable them to be used within the make_stems algorithm
+#' @param comorbid_column A vector of character strings made up of numbers 0-9 or of factors coercible to character, all should be identical lengths.
+#' @param dis_names A list of disease names associated with each position as used in \code{\link{get_disease_counts}}
+#'
+#' @returns A list, the first element of which is a data.frame which includes \code{master_str}, which is can be joined with original data.
+#' @examples
+#'
+#' @export
 
-#flow is if else loop at top of all funs --> if the string includes any digits >1, and N is max;
-# turn into 0-N and replace, then remake string
-# e.g. 300 - CKD/DM/HF, becomes 00100, CKD1,CKD2,CKD3,DM,HF?
-# two things would need to change here, disease_names and the comorbid_column
-# this needs to be as the first step of make_stems
+#' a<-c('3003', '2100', '3103', '0114')
 
-#a<-c('3003', '2100', '3103', '0114')
+multiple_state_processor <- function(comorbid_column, dis_names = NULL){
+  if(!check_strings_equal(comorbid_column)){stop('comorbid column must be the same length for each record')}
 
-identify_multiple_states <-function(comorbid_columns){
-  # staged identification
-  # find any strings with non-numeric, these should stop
-  if(any(grepl('[^0-9]',comorbid_columns) == TRUE))stop('There are non-numeric characters in the comorbid strings, please remove')
-
-  ##find those that aren't 0-1
-  if(any(grepl('[^0-1]', comorbid_columns) == TRUE))
-    # If we find non 0-1 --> multiple states
-    {return('multiple')}
-
-  else
-    # Otherwise it is single state
-    {return('single')}
-}
-
-multiple_state_processor <- function(comorbid_column, disease_names = NULL){
-  # if multiple states then this runs
-
+  comorbid_column <- to_string(comorbid_column)
   # number of items in string
   n <- nchar(comorbid_column[1])
 
@@ -56,7 +43,14 @@ multiple_state_processor <- function(comorbid_column, disease_names = NULL){
 
     tt$str <- paste0(rep('0', max_n), collapse='')
 
-    for(position in 1:length(str_positions)){
+    if(max_n == 0){
+
+      cat("empty column - I'll fill it with 0")
+      tt$str <- paste0(rep('0', 1), collapse='')
+
+      }else{
+
+        for(position in 1:length(str_positions)){
 
       rows <- str_positions[[position]]
 
@@ -69,21 +63,34 @@ multiple_state_processor <- function(comorbid_column, disease_names = NULL){
       substr(int_str, position, position) <- '1'
       tt[rows, 'str'] <- int_str
     }
-
+    }
     tt[,'master_str'] <- apply(tt, 1, function(x) paste0(x['master_str'], x['str']))
 
     tt$str <- NULL
   }
-  if(!is.null(disease_names)){
-    if(length(disease_names) != n)stop('Disease names should be the same length as comorbid string')
-    d_name <- rep(disease_names, max_by_po)
+  if(!is.null(dis_names)){
+    if(length(dis_names) != n)stop('Disease names should be the same length as comorbid string')
+    d_name <- rep(dis_names, max_by_po)
     numbering <- lapply(max_by_po, function(x) 1:x)
     numbers_ready <- Reduce(c, numbering)
     new_names <- do.call(paste0, list(d_name, numbers_ready))
     return(list(tt, new_names))
   }
-  cat('If you provide column names I will reassign them for you')
-  # This is an intermediary table where each string has it's new string assigned
-  # it can be used to join back to core comorbid_columns
+  cat('If you provide column names I will reassign them for you \n')
   return(tt)
+}
+
+.identify_multiple_states <-function(comorbid_columns){
+  # staged identification
+  # find any strings with non-numeric, these should stop
+  if(any(grepl('[^0-9]',comorbid_columns) == TRUE))stop('There are non-numeric characters in the comorbid strings, please remove')
+
+  ##find those that aren't 0-1
+  if(any(grepl('[^0-1]', comorbid_columns) == TRUE))
+    # If we find non 0-1 --> multiple states
+  {return('multiple')}
+
+  else
+    # Otherwise it is single state
+  {return('single')}
 }
--- tests/testthat/test_multiple_states.R
+++ tests/testthat/test_multiple_states.R
@@ -1,5 +1,12 @@
 context('test multiple_states functionality')
 
-test_that(,{})
+test_that('data entry appropriate',{
+  factor_str <- factor(c('0101', '3000', '3100'))
+  expect_equal(multiple_state_processor(t)[[2]], NA)
+          })
 
-test_that(,{})
+test_that('multiple_states function works appropriately',{})
+
+test_that('adding dis_names works appropriately',{})
+
+test_that('edge case failures',{})
